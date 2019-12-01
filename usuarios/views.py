3cf44f2176ae8ee04e059c7a265ca433c987802f
Thiago Brasil <tchaguitos@gmail.com>
2019-11-30 23:32:17 -0300

    feat(usuarios): altera view para trabalhar com template
    Adiciona o template "index.html" e altera a view index no aplicativo usuarios para utilizar o template criado (index.html)

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello world")
+    return render(request, "index.html", {})
+
