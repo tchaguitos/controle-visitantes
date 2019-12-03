a98a6a1551dfee543ae056ab159b3d1526422d6b
Thiago Brasil <tchaguitos@gmail.com>
2019-12-03 20:48:50 -0300

    feat(visitantes): estiliza formulário com django-widget-tweaks

--- visitantes/forms.py
+++ visitantes/forms.py
@@ -5,4 +5,8 @@ class VisitanteForm(forms.ModelForm):
     
     class Meta:
         model = Visitante
-        fields = "__all__"
+        fields = [
+            "nome_completo", "cpf", "data_nascimento",
+            "numero_casa", "placa_veiculo", "autorizado_por",
+        ]
+
