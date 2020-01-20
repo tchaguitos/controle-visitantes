a266e41e6ea0a5d5ea7201e0ca8e44f05e4f6cf7
Thiago Brasil <tchaguitos@gmail.com>
2020-01-20 09:46:22 -0300

    feat(visitantes): add formulário para autorizar visitante

--- visitantes/forms.py
+++ visitantes/forms.py
@@ -1,10 +1,19 @@
 from django import forms
 from visitantes.models import Visitante
 
+
 class VisitanteForm(forms.ModelForm):
     class Meta:
         model = Visitante
         fields = [
             "nome_completo", "cpf", "data_nascimento",
             "numero_casa", "placa_veiculo", "autorizado_por",
+        ]
+
+
+class AutorizaVisitanteForm(forms.ModelForm):
+    class Meta:
+        model = Visitante
+        fields = [
+            "status", "autorizado_por", "horario_autorizacao"
         ]
