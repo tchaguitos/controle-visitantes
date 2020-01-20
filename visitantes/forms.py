18c3e7c16fcff2f87507085256fd83b5b593c5af
Thiago Brasil <tchaguitos@gmail.com>
2020-01-20 19:09:21 -0300

    feat(visitantes): add error_messages ao formulário VisitanteForm

--- visitantes/forms.py
+++ visitantes/forms.py
@@ -6,11 +6,28 @@ class VisitanteForm(forms.ModelForm):
         model = Visitante
         fields = [
             "nome_completo", "cpf", "data_nascimento",
-            "numero_casa", "placa_veiculo", "autorizado_por",
+            "numero_casa", "placa_veiculo",
         ]
+        error_messages = {
+            "nome_completo": {
+                "required": "O nome completo do visitante é obrigatório para cadastro",
+            },
+            "cpf": {
+                "required": "O CPF do visitante é obrigatório para cadastro"
+            },
+            "data_nascimento": {
+                "required": "A data de nascimento do visitante é obrigatória para cadastro",
+                "invalid": "Por favor, informe um formato válido para a data de nascimento (DD/MM/AAAA)"
+            },
+            "numero_casa": {
+                "required": "Por favor, informe o número da casa a ser visitada"
+            },
+        }
 
 
 class AutorizaVisitanteForm(forms.ModelForm):
+    autorizado_por = forms.CharField(required=True)
+
     class Meta:
         model = Visitante
         fields = [
