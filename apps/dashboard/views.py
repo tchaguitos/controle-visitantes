342d6ce5d7ebe59921571b1f8d678c39b23015e7
Thiago Brasil <tchaguitos@gmail.com>
2020-01-22 20:46:22 -0300

    feat(dashboard): add paginação de visitantes

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from visitantes.models import Visitante
 
 from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
 
 from datetime import datetime
 
@@ -11,9 +12,7 @@ def index(request):
 
     visitantes = Visitante.objects.all().order_by("-horario_chegada")
 
-    hora_atual = datetime.now()
-    mes_atual = hora_atual.month
-
+    # filtrando visitantes por status
     visitantes_em_visita = visitantes.filter(
         status="EM_VISITA"
     ).count()
@@ -26,9 +25,18 @@ def index(request):
         status="FINALIZADO"
     ).count()
 
+    # filtrando visitantes por data (mês atual)
+    hora_atual = datetime.now()
+    mes_atual = hora_atual.month
+
     visitantes_mes = visitantes.filter(
         horario_chegada__month=mes_atual
     ).count()
+
+    # paginacao
+    numero_pagina = request.GET.get('page', 1)
+    visitantes_paginados = Paginator(visitantes, 10)
+    pagina_obj = visitantes_paginados.get_page(numero_pagina)
 
     context = {
         "nome_pagina": "Página inicial",
@@ -36,7 +44,8 @@ def index(request):
         "visitantes_em_visita": visitantes_em_visita,
         "visitantes_aguardando": visitantes_aguardando,
         "visitantes_finalizado": visitantes_finalizado,
-        "visitantes_mes": visitantes_mes
+        "visitantes_mes": visitantes_mes,
+        "pagina_obj": pagina_obj
     }
 
     return render(request, "index.html", context)
