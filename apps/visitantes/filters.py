3790ed39cbd1a41fafaa181257a8796fa23133a0
Thiago Brasil <tchaguitos@gmail.com>
2020-01-23 15:24:47 -0300

    feat(visitantes): torna possível pesquisar por parte do nome

--- apps/visitantes/filters.py
+++ apps/visitantes/filters.py
@@ -4,6 +4,8 @@ from visitantes.models import Visitante
 
 
 class VisitanteFilter(django_filters.FilterSet):
+    nome_completo = django_filters.CharFilter(lookup_expr="icontains")
+
     class Meta:
         model = Visitante
         fields = [
