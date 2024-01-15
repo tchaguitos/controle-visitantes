9454b4eebb166c943f0fed525d964042687e111e
Thiago Brasil <tchaguitos@gmail.com>
2024-01-15 20:58:28 -0300

    feat(api): add endpoint para cadastrar visitantes

--- apps/api/views.py
+++ apps/api/views.py
@@ -6,7 +6,7 @@ from django.shortcuts import get_object_or_404
 from django.db import IntegrityError, InternalError
 
 from visitantes.models import Visitante
-from visitantes.serializers import VisitanteSerializer
+from visitantes.serializers import CriarVisitanteSerializer, VisitanteSerializer
 
 class VisitanteAPI(viewsets.ViewSet):
     def get_queryset(self):
@@ -27,3 +27,19 @@ class VisitanteAPI(viewsets.ViewSet):
         serializer = VisitanteSerializer(visitante)
 
         return Response(serializer.data)
+
+    def create(self, request):
+        serializer = CriarVisitanteSerializer(data=request.data)
+
+        if serializer.is_valid():
+            visitante = serializer.save(registrado_por=request.user.porteiro)
+
+            return Response(
+                {"token": visitante.token},
+                status=status.HTTP_201_CREATED
+            )
+
+        return Response(
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
--- apps/visitantes/serializers.py
+++ apps/visitantes/serializers.py
@@ -2,6 +2,14 @@ from rest_framework import serializers
 
 from visitantes.models import Visitante
 
+class CriarVisitanteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Visitante
+        fields = [
+            "nome_completo", "cpf", "data_nascimento",
+            "numero_casa", "placa_veiculo",
+        ]
+
 class VisitanteSerializer(serializers.ModelSerializer):
     cpf = serializers.CharField(source="get_cpf")
     registrado_por = serializers.StringRelatedField(many=False)
